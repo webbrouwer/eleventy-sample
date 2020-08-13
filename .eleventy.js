a79f2cf531f41b1f83178742448caa2b4c9ec0a7
webbrouwer <mickey@webbrouwer.com>
2020-08-13 23:35:24 +0200

    added slider

--- .eleventy.js
+++ .eleventy.js
@@ -4,4 +4,10 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addFilter("cssmin", function(code) {
         return new CleanCSS({}).minify(code).styles;
     });
+
+    eleventyConfig.addPassthroughCopy({
+        "node_modules/tiny-slider/dist/tiny-slider.css": "tiny-slider.css",
+        "node_modules/tiny-slider/dist/min/tiny-slider.js": "tiny-slider.js"
+    });
+
 };
